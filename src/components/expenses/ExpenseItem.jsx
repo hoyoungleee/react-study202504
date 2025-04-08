1d75d653c2218657c2ec2e0e7fb825e5d98f5553
hoyoungleee <ghdud939@gmail.com>
2025-04-08 14:15:26 +0900

    컴포넌트 디자인 추가

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 // 전달되는 props는 객체로 옵니다. 디스트럭쳐링 등의 문법으로 쪼개서 사용할 수 있습니다.
 const ExpenseItem = ({ title, price, date }) => {
   return (
     <div className='expense-item'>
-      <div>{date.toLocaleString()}</div>
+      <ExpenseDate date={date}/>
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{price}원</div>
